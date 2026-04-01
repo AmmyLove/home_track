338504051a66a782f39f9a9d5504c736bc89a952
ammy love <ammylove182@gmail.com>
2026-04-01 19:40:33 +0100

    modified the config file

--- server/config/database.js
+++ server/config/database.js
@@ -3,22 +3,15 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    logging: false, // set to console.log if you want to see SQL queries
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false // This is important for Render
-      }
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
   }
-);
-
+});
 
 export default sequelize;
